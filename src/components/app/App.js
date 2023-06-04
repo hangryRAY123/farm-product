4d080b1869a6d959abfec01cfbdbbac98f656b2a
Павел <pasha.seregin.92@mail.ru>
2023-06-04 16:20:18 +0300

    ref

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import { GlobalStyle } from './styles';
 import { PageWrapper } from '../layout/page-wrapper/PageWrapper';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 export const App = () => {
   return (
